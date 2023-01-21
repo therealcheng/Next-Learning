2e2c2cc89809e256fa80f3e212e790a510cdd636
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-01-21 14:02:10 +0000

    Learning Custom 404 Page

--- hello-world/pages/index.js
+++ hello-world/pages/index.js
@@ -1,11 +1,20 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function Home({ title }) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log('Placing your order');
+    router.push('./product');
+  };
+
   return (
     <div>
       <h1>{title}</h1>
       <Link href='/blog'>Blog</Link>
       <Link href='/product'>Product</Link>
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 }
